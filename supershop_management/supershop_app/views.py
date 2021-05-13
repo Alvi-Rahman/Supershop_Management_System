8edac6ce8264cc25e0ca01bd147e2de8a999c624
Alvi-Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2021-05-13 13:31:41 +0600

    add: urls and templates

--- supershop_management/supershop_app/views.py
+++ supershop_management/supershop_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+
+class HomePage(View):
+    template_name = "supershop_app/index.html"
+
+    def get(self, request):
+        # paraphrase_class = ParaphraseTool()
+        return render(request, self.template_name, context={'get': 1})
+
+    def post(self, request):
+        return render(request, self.template_name)

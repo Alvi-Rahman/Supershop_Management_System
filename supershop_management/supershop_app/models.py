80e842237e95dcecd3e82a938990af174c77dddc
Alvi-Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2021-05-14 10:09:56 +0600

    update: __str__ in ProductCategory

--- supershop_management/supershop_app/models.py
+++ supershop_management/supershop_app/models.py
@@ -21,6 +21,9 @@ class ProductCategory(models.Model):
     category_code = models.UUIDField(default=uuid.uuid4)
     category_name = models.CharField(max_length=255, blank=False, null=False)
 
+    def __str__(self):
+        return self.category_name
+
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True)

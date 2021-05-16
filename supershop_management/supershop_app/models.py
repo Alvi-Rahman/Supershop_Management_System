fb8b85b869ed48a22b268d4f2db5770c2521b314
Alvi-Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2021-05-16 08:46:35 +0600

    add: order_placed in Order model

--- supershop_management/supershop_app/models.py
+++ supershop_management/supershop_app/models.py
@@ -42,7 +42,9 @@ class Order(models.Model):
     order_id = models.UUIDField(default=uuid.uuid4)
     purchased_products = models.ManyToManyField(Product)
     purchase_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_placed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.order_id
+
 

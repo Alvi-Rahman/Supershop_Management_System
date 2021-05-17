e8de0dc9787d78698272e8c575ab7e3eed766eda
Alvi-Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2021-05-18 02:04:56 +0600

    add: Cart Model

--- supershop_management/supershop_app/models.py
+++ supershop_management/supershop_app/models.py
@@ -37,6 +37,15 @@ class Product(models.Model):
         return self.product_name
 
 
+class Cart(models.Model):
+    id = models.AutoField(primary_key=True)
+    cart_code = models.UUIDField(default=uuid.uuid4)
+    added_products = models.ForeignKey(Product, on_delete=models.SET_NULL, db_constraint=False, null=True)
+
+    def __str__(self):
+        return str(self.cart_code)
+
+
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
     order_id = models.UUIDField(default=uuid.uuid4)
